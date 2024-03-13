bdf799f625da2f2939276d31a73daf3638ae46fd
ksj1368 <ksj006151@gmail.com>
2024-03-13 14:17:37 +0900

    add: yaml 파일 별 전처리 프로세스 추가

--- spark_ETL/etl.py
+++ spark_ETL/etl.py
@@ -1,10 +1,10 @@
 
 from pyspark.sql import SparkSession
-import pandas as pd
 from pandas_gbq import gbq
 from google.oauth2 import service_account
 from google.cloud import bigquery
-from auth_bigquery import authorize_bigquery
+import process_yaml_data as pyd
+
 
 class ETL():
     # spark session 생성
@@ -24,25 +24,19 @@ class ETL():
         return df
 
     # 전처리 함수
-    def transform(self, view_id, query, data_path, num_partitions):
-        df = self.extract(data_path, num_partitions)
+    def transform(self, df, view_id, query,):
         df.createOrReplaceTempView(view_id)
         # self.spark.catalog.cacheTable(view_id)
         df = self.spark.sql(query)
         # self.spark.catalog.uncacheTable(view_id)
         df = df.toPandas().dropna()
-        df.head(3)
-        
-        # df.rdd.getNumPartitions() # 사용안할 경우: 22.70798945426941, 사용할 경우: 22.165385007858276 하지만 메모리 문제 발생
+        print("columns: ", df.columns)
+        # df.rdd.getNumPartitions(3) # 사용안할 경우: 22.70798945426941, 사용할 경우: 22.165385007858276 하지만 메모리 문제 발생
         # 캐시 생성을 안할 경우에 연산 속도가 2초 빠름
         print("transform Done.")
         return df
         
-    def load(self, view_id, query, data_path, num_partitions, dataset_id, table_id):
-        credentials = authorize_bigquery()
-        
-        df = self.transform(view_id, query, data_path, num_partitions)
-        
+    def load(self, df, dataset_id, table_id, view_id, credentials):
         key_file_path = "/home/ksj0061/airflow-tutorial/teemo-415918-414755ce7c80.json"
         
         # Create BigQuery client
@@ -52,20 +46,16 @@ class ETL():
         # 빅쿼리 테이블
         project_id = credential.project_id
         dataset_id = dataset_id
-        table_id = table_id
-
-        df = df[:100] # 나중에 제거하기
-
-        # unlist
-        df = df.explode(["champ_id", "play"]).reset_index(drop=True) # df 탐색후 첫 번째 열을 for문으로 돌려 타입 검사 -> 리스트일 경우 explode, obj일 경우 str
-
-        # 데이터 타입 변경
-        df["champ_id"] = df["champ_id"].astype(int)
-        df["play"] = df["play"].astype(int)
-        df["summoner_id"] = df["summoner_id"].astype(str)
-        
+        table_id = "test_table"
+        if view_id == "summoner_info":
+            print(f"view_id: {view_id}")
+            df = pyd.process_summoner_data(df)
+        elif view_id == "match_info":
+            print(f"view_id: {view_id}")
+            df = pyd.process_match_data(df)
+            
         gbq.to_gbq(df, destination_table= f"{dataset_id}.{table_id}", credentials=credentials, project_id=project_id, if_exists="append")
-
+        
         # PySpark 세션 종료
         self.spark.stop()
         print("load Done.")
