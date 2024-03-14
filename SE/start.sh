2bb8a19fdf890c8e51f1eaf7c858ec2fd46f6422
kyungwon <power16one5@gmail.com>
2024-03-14 22:21:35 +0900

    wip

--- SE/start.sh
+++ SE/start.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 # 원격 서버 접속 정보
-SERVERS=("bgw-server" "ngo-server" "lsg-server" "jsj-server")
+# SERVERS=("bgw-server" "ngo-server" "lsg-server" "jsj-server")
+SERVERS=("bgw-server" "ngo-server" "lsg-server")
 
 # SSH를 사용하여 원격 서버에 접속하고 명령어 실행
 for i in $(seq 0 $((${#SERVERS[@]} - 1))); do
-	COMMANDS='cd gpu-cluster;
+	COMMANDS="cd gpu-cluster;
 	source multi-gpu/bin/activate;
 	cd level2-3-recsys-finalproject-recsys-05;
 	git checkout cossim;
 	git pull;
 	cd SE;
-	nohup torchrun --nnodes=${#SERVERS[@]} --nproc_per_node=1 --node_rank=$i --master_addr=10.0.2.7 --master_port=20000 run.py &;'
+	# pip install -r requirement.txt;
+	pkill -9 -u bgw torchrun;
+	# nohup torchrun --nnodes=${#SERVERS[@]} --nproc_per_node=1 --node_rank=$i --master_addr=10.0.2.7 --master_port=20000 run.py > nohup.out 2>&1 &
+	"
 	
-	ssh ${SERVERS[i]} "$(echo -e $COMMANDS)"
+	ssh -n ${SERVERS[i]} "$(echo -e $COMMANDS)"
 done
