c6d7492755c4c63c63319283e194705bc2538444
kyungwon <power16one5@gmail.com>
2024-03-15 15:40:42 +0900

    wip

--- SE/start.py
+++ SE/start.py
@@ -14,48 +14,28 @@ parser.add_argument("--lr", type=str, required=True)
 args = parser.parse_args()
 
 # 원격 서버 접속 정보
-master = "bgw-server"
-worker = ["ngo-server", "lsg-server", "jsj-server"]
+servers = ["bgw-server", "ngo-server", "lsg-server", "jsj-server"]
 
-# SSH 접속 설정
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-for i, server in enumerate(worker, start=1):
-    try:
-        # SSH 접속
-        ssh.connect(server)
-        scp = SCPClient(ssh.get_transport())
+def connect_and_execute(server, commands, copy=False):
+    # 새로운 SSHClient 인스턴스 생성
+    with paramiko.SSHClient() as ssh:
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            ssh.connect(server)
+            with SCPClient(ssh.get_transport()) as scp:
+                # 필요한 작업 수행...
+                if copy:
+                    scp.put('../../../data/', recursive=True, remote_path='gpu-cluster/')
 
-        # 데이터 디렉토리 전송
-        scp.put('../../../data/', recursive=True, remote_path='gpu-cluster/')
+                stdin, stdout, stderr = ssh.exec_command(commands)
+                # 결과 처리...
+        except paramiko.ssh_exception.NoValidConnectionsError as e:
+            print(f"Connection failed for {server}: {e}")
 
-        # 원격 명령어 실행
-        commands = f"""
-        cd gpu-cluster;
-        source multi-gpu/bin/activate;
-        cd level2-3-recsys-finalproject-recsys-05;
-        git checkout cossim;
-        git pull;
-        cd SE;
-        nohup torchrun --nnodes={len(worker) + 1} --nproc_per_node=1 --node_rank={i} --master_addr=10.0.2.7 --master_port=20000 \
-        run.py --hidden_size={args.hidden_size} --emb_size={args.emb_size} --dropout={args.dropout} --lr={args.lr} > nohup.out 2>&1 &
-        """
-        stdin, stdout, stderr = ssh.exec_command(commands)
 
-    finally:
-        if ssh:
-            ssh.close()
-
-try:
-    # SSH 접속
-    ssh.connect(master)
-    scp = SCPClient(ssh.get_transport())
-
-    # 데이터 디렉토리 전송
-    scp.put('../../../data/', recursive=True, remote_path='gpu-cluster/data')
-
-    # 원격 명령어 실행
+# 사용 예
+for i, server in enumerate(servers):
     commands = f"""
     cd gpu-cluster;
     source multi-gpu/bin/activate;
@@ -63,46 +43,24 @@ try:
     git checkout cossim;
     git pull;
     cd SE;
-    torchrun --nnodes={len(worker) + 1} --nproc_per_node=1 --node_rank=0 --master_addr=10.0.2.7 --master_port=20000 \
-    run.py --hidden_size={args.hidden_size} --emb_size={args.emb_size} --dropout={args.dropout} --lr={args.lr} &
+    nohup torchrun --nnodes={len(servers)} --nproc_per_node=1 --node_rank={i} --master_addr=10.0.2.7 --master_port=20000 \
+    run.py --hidden_size={args.hidden_size} --emb_size={args.emb_size} --dropout={args.dropout} --lr={args.lr} > nohup.out 2>&1 &
     """
-    stdin, stdout, stderr = ssh.exec_command(commands)
-    print(stdout.read().decode())
-    print(stderr.read().decode())
+    connect_and_execute(server, commands, copy=True)
 
-finally:
-    if ssh:
-        ssh.close()
 
 
 time.sleep(10)
-for server in [master] + worker:
-    try:
-        # SSH 접속
-        ssh.connect(server)
+for server in servers:
+    commands = f"""
+    pkill -15 -u bgw torchrun;
+    """
+    connect_and_execute(server, commands)
 
-        # 원격 명령어 실행
-        commands = f"""
-        pkill -15 -u bgw torchrun;
-        """
-        stdin, stdout, stderr = ssh.exec_command(commands)
-
-    finally:
-        if ssh:
-            ssh.close()
 
 time.sleep(10)
-for server in [master] + worker:
-    try:
-        # SSH 접속
-        ssh.connect(server)
-
-        # 원격 명령어 실행
-        commands = f"""
-        pkill -9 -u bgw torchrun;
-        """
-        stdin, stdout, stderr = ssh.exec_command(commands)
-
-    finally:
-        if ssh:
-            ssh.close()
+for server in servers:
+    commands = f"""
+    pkill -9 -u bgw torchrun;
+    """
+    connect_and_execute(server, commands)
